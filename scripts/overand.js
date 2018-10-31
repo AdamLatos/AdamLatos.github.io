e58cdbc72506b559a9a64bac2e75c7706bdcebfe
Adam <adam.t.latos@gmail.com>
2018-11-01 00:15:30 +0100

    fuck

--- scripts/overand.js
+++ scripts/overand.js
@@ -1,7 +1,9 @@
 var canvas = document.getElementById('myCanvas')
 var ctx = canvas.getContext('2d');
-ctx.translate(0,canvas.height); 
-ctx.scale(1,-1);
+var ctxtext = canvas.getContext('2d');
+//ctx.translate(0,canvas.height); 
+//ctx.scale(1,-1);
+
 
 function init() {
     window.requestAnimationFrame(draw);
@@ -9,10 +11,10 @@ function init() {
 
 var player = {
     x: 40,
-    y: 80,
+    y: 400,
     dx: 0,
     dy: 0,
-    size: 40,
+    size: 48,
     color: "black",
     draw: function () {
         ctx.beginPath();
@@ -20,6 +22,16 @@ var player = {
         ctx.closePath();
         ctx.fillStyle = this.color;
         ctx.fill();
+    },
+    win: 0,
+    reset: function () {
+        this.x = 40,
+        this.y = 400,
+        this.dx = 0,
+        this.dy = 0,
+        this.size = 48,
+        this.color = "black",
+        this.win = 0;
     }
 }
 
@@ -28,12 +40,77 @@ var floor = {
     color: "gray",
     draw: function() {
         ctx.beginPath();
-        ctx.rect(0, 0, 800, 40);
+        ctx.rect(0, 600, 800, 40);
         ctx.closePath();
         ctx.fillStyle = this.color;
         ctx.fill();
     }
 }
+
+var tile = {
+    color: "blue",
+    x: 0,
+    y: 0,
+    tsize: 48,
+    draw: function() {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.tsize, this.tsize);
+        ctx.closePath();
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+var tilemap = {
+    rows: 10,
+    cols: 20,
+    tsize: 48,
+    layers: [[
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 1, 1, 1, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 1, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 1, 0, 0, 0, 0,  0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 1, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 1, 1, 1, 1, 1,  1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+
+    ]],
+    getTile: function(layer, row, col) {
+        return this.layers[layer][row * tilemap.cols + col];
+    },
+
+    drawLayer: function(layer) {
+        for (var c = 0; c < this.cols; c++) {
+            for (var r = 0; r < this.rows; r++) {
+                var newTile = tilemap.getTile(layer, r, c);
+                switch(newTile) {
+                    case 1:
+                        tile.color = "DarkSlateGray"
+                        tile.x = c*this.tsize;
+                        tile.y = r*this.tsize;
+                        tile.tsize = this.tsize;
+                        tile.draw();
+                        break;
+                    case 2:
+                        tile.color = "DarkGreen"
+                        tile.x = c*this.tsize;
+                        tile.y = r*this.tsize;
+                        tile.tsize = this.tsize;
+                        tile.draw();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
+}
+
+
 
 var controls = {
     rightPressed: false,
@@ -81,33 +158,77 @@ function keyUpHandler(e) {
     }
 }
 
-function checkCollision() {
-    if(player.y > 40){
-        player.dy -= 2;
-    } else {
+function movePlayer() {
+
+    var newX = player.x + player.dx;
+    var newY = player.y //+ player.dy;
+
+    //check against 4 tiles
+    X1 = Math.floor(newX/tilemap.tsize);
+    X2 = Math.ceil(newX/tilemap.tsize);
+    Y1 = Math.floor(newY/tilemap.tsize);
+    Y2 = Math.ceil(newY/tilemap.tsize);
+
+    var upLeft = tilemap.getTile(0,Y1,X1);
+    var upRight = tilemap.getTile(0,Y1,X2);
+    var downLeft = tilemap.getTile(0,Y2,X1);
+    var downRight = tilemap.getTile(0,Y2,X2);
+
+    var win = 0;
+
+    // win condition
+    if(downLeft == 2 || downRight == 2 || upLeft == 2 || upRight == 2){
+        win = 1;
+    }
+
+    // right collision
+    if(upRight == 1){
+        player.dx = 0;
+        newX = X1*tilemap.tsize;
+    }
+
+    // left collision
+    if(upLeft == 1){
+        player.dx = 0;
+        newX = X2*tilemap.tsize;
+    }
+
+    var newY = player.y + player.dy;
+
+    X1 = Math.floor(newX/tilemap.tsize);
+    X2 = Math.ceil(newX/tilemap.tsize);
+    Y1 = Math.floor(newY/tilemap.tsize);
+    Y2 = Math.ceil(newY/tilemap.tsize);
+    var upLeft = tilemap.getTile(0,Y1,X1);
+    var upRight = tilemap.getTile(0,Y1,X2);
+    var downLeft = tilemap.getTile(0,Y2,X1);
+    var downRight = tilemap.getTile(0,Y2,X2);
+
+    // gravity
+    if(downLeft == 1 || downRight == 1){
         player.dy = 0;
+        newY = Y1*tilemap.tsize;
+        if(controls.upPressed){
+            player.dy = -25;
+        }
+    } else {
+        player.dy += 2;
     }
-    if(player.x < 0){
-        player.x = 0;
-    } else if (player.x + player.size > canvas.width) {
-        player.x = canvas.width - player.size
+
+    // head collision
+    if(upLeft == 1 || upRight == 1){
+        player.dy = 2;
+        newY = Y2*tilemap.tsize;
     }
-    if(player.y < 40){
-        player.y = 40;
-    } else if (player.y + player.size > canvas.height) {
-        player.y = canvas.height - player.size
+
+
+
+    if(newX < 0){
+        player.dx = 0;
+        newX = 0;
     }
-}
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    floor.draw();
-    player.draw();
-
-
-
-
-
+    // right/left movement
     if(controls.rightPressed){
         player.x += 4;
         player.dx += 2;
@@ -115,9 +236,6 @@ function draw() {
     if(controls.leftPressed){
         player.x -= 4;
         player.dx -= 2;
-    }
-    if(controls.upPressed && player.y == 40){
-        player.dy += 25;
     }
 
     if(player.dx < 0) {
@@ -132,11 +250,35 @@ function draw() {
         player.dx = 10;
     }
 
-    player.x += player.dx;
-    player.y += player.dy;
+    player.x = newX;
+    player.y = newY;
 
-    checkCollision();
+    if(win == 1){
+        ctx.font = "100px Arial";
+        ctx.fillStyle = "Green";
+        ctx.textAlign="center"; 
+        ctx.fillText("YOU WON!",320,150);
+        ctx.font = "20px Arial"
+        ctx.fillText("to restart press space",320,200);
+        return 1;
+    }
+    return 0;
+}
 
+function draw() {
+    if (player.win){
+        if(controls.spacePressed == 1){
+            player.reset();
+            player.win = 0;
+        }
+    }
+    else {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //floor.draw();
+    tilemap.drawLayer(0);
+    player.win = movePlayer();
+    player.draw();
+    }
     window.requestAnimationFrame(draw);
 }
 
